bc324efbf8a222a4c71aab392cb1e53896f4a7d3
Abhilash Achugatla <aachu001@odu.edu>
2020-08-20 12:22:42 -0400

    presentational and container components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -55,13 +55,15 @@ export class DishDetail extends Component{
     render(){
         if(this.props.dish != null)
             return (
-                <div className="row">
-                <div  className="col-12 col-md-5 m-1">
-                   {this.renderDish(this.props.dish)} 
-                </div>
-                <div  className="col-12 col-md-5 m-1">
-                   {this.renderComments(this.props.dish)} 
-                </div>
+                <div className = "container">
+                    <div className="row">
+                        <div  className="col-12 col-md-5 m-1">
+                            {this.renderDish(this.props.dish)} 
+                        </div>
+                        <div  className="col-12 col-md-5 m-1">
+                            {this.renderComments(this.props.dish)} 
+                        </div>
+                    </div>
                 </div>
                
             
