45dc13f3cbcd4a7dd912401d98154aac6377ed59
Abhilash Achugatla <aachu001@odu.edu>
2020-09-02 00:11:39 -0400

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { Card, CardImg, CardText, CardBody,CardTitle, Breadcrumb, BreadcrumbItem,Button,Modal, ModalHeader, ModalBody,Label,Row,Col } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent'; 
 
 
 
@@ -150,9 +151,25 @@ class CommentForm extends Component{
 }
 
 const DishDetail =(props) => {
-    
-    console.log('Dishdetail component  render invoked');
-    if (props.dish != null)
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null)
         return (
             <div className="container">
                 <div className="row">
